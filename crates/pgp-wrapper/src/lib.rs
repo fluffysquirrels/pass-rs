fa77917b6ab40ff683e6a1d47910dbbcbeb0e64c
Alex Helfet <alex.helfet@gmail.com>
2023-01-27 13:09:10 +0000

    WIP for encrypt().

--- crates/pgp-wrapper/src/lib.rs
+++ crates/pgp-wrapper/src/lib.rs
@@ -4,31 +4,70 @@
 #[macro_use]
 mod pgp_wrapper_world;
 
+use core::num::NonZeroU32;
+use getrandom::register_custom_getrandom;
+use pgp::{
+    Deserializable,
+    Message,
+    SignedPublicKey,
+    SignedSecretKey,
+    crypto::sym::SymmetricKeyAlgorithm,
+    ser::Serialize,
+};
 use pgp_wrapper_world::{
+    get_random,
     pgp_wrapper_exports,
     __link_section,
 };
 pub use pgp_wrapper_world::pgp_wrapper_exports::PgpWrapperExports;
 
-pub fn getrandom_always_fail(_: &mut [u8]) -> Result<(), getrandom::Error> {
-    unimplemented!()
+fn getrandom_with_import(out: &mut [u8]) -> Result<(), getrandom::Error> {
+    let count = out.len();
+    let count_u32: u32 = count.try_into().map_err(|_e| getrandom::Error::from(NonZeroU32::new(getrandom::Error::CUSTOM_START).expect("NonZeroU32::new")))?;
+    let bytes = get_random::get_random_bytes(count_u32);
+    out.copy_from_slice(&*bytes);
+    Ok(())
 }
-use getrandom::register_custom_getrandom;
-register_custom_getrandom!(getrandom_always_fail);
 
-use pgp::{ Deserializable, Message, SignedSecretKey };
+register_custom_getrandom!(getrandom_with_import);
+
+struct ImportRng;
+
+impl rand_core::RngCore for ImportRng {
+    fn next_u32(&mut self) -> u32 {
+        rand_core::impls::next_u32_via_fill(self)
+    }
+
+    fn next_u64(&mut self) -> u64 {
+        rand_core::impls::next_u64_via_fill(self)
+    }
+
+    fn fill_bytes(&mut self, dest: &mut [u8]) {
+        let count = dest.len();
+        let count_u32: u32 = count.try_into().expect("ImportRng::fill_bytes expected to convert len to u32");
+        let bytes = get_random::get_random_bytes(count_u32);
+        dest.copy_from_slice(&*bytes);
+    }
+
+    fn try_fill_bytes(&mut self, dest: &mut [u8]) -> Result<(), rand_core::Error> {
+        self.fill_bytes(dest);
+        Ok(())
+    }
+}
+
+impl rand_core::CryptoRng for ImportRng {}
 
 pub struct RealPgpWrapperWorld;
 
 impl PgpWrapperExports for RealPgpWrapperWorld {
     fn decrypt(
         encrypted_message: Vec<u8>,
-        key: String,
+        priv_key_bytes: Vec<u8>,
         passphrase: String
     ) -> Result<Vec<u8>, String> {
         let enc_message = Message::from_bytes(&*encrypted_message)
             .map_err(|e| e.to_string())?;
-        let (priv_key, _) = SignedSecretKey::from_string(&*key)
+        let priv_key = SignedSecretKey::from_bytes(&*priv_key_bytes)
             .map_err(|e| e.to_string())?;
         let (decrypter, _) = enc_message.decrypt(
             /* key_pw */ || passphrase,
@@ -44,11 +83,30 @@ impl PgpWrapperExports for RealPgpWrapperWorld {
         let decrypted_message = dec_result.as_ref().map_err(|e| e.to_string())?;
         let literal_data =
             decrypted_message.get_literal()
-            .ok_or("No literal in decrypted message".to_string())?;
+                             .ok_or("No literal in decrypted message".to_string())?;
         // let str = literal_data.to_string()
         //                       .ok_or("Non UTF-8 string in LiteralData".to_string())?;
         let dec_bytes = literal_data.data().to_vec();
         Ok(dec_bytes)
+    }
+
+    fn encrypt(
+        message_bytes: Vec<u8>,
+        pub_key_bytes: Vec<u8>
+    ) -> Result<Vec<u8>, String> {
+        let message = Message::new_literal_bytes(/* file_name */ "pass-rs", &*message_bytes);
+        let pub_key = SignedPublicKey::from_bytes(&*pub_key_bytes)
+                                      .map_err(|e| e.to_string())?;
+        let mut rng = ImportRng;
+        let enc_message =
+            message.encrypt_to_keys(
+                &mut rng,
+                SymmetricKeyAlgorithm::AES256,
+                &[&pub_key]
+            ).map_err(|e| e.to_string())?;
+        let enc_bytes = enc_message.to_bytes()
+            .map_err(|e| e.to_string())?;
+        Ok(enc_bytes)
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
-use anyhow::anyhow;
+use rand::RngCore;
 
 wasmtime::component::bindgen!("pgp-wrapper");
 
 struct Context;
 
-impl pgp_wrapper_imports::PgpWrapperImports for Context {}
+impl get_random::GetRandom for Context {
+    fn get_random_bytes(&mut self, len: u32) -> Result<Vec<u8>, anyhow::Error> {
+        let mut ret = Vec::with_capacity(len as usize);
+        ret.resize(len as usize, 0);
+        rand::thread_rng().fill_bytes(&mut *ret);
+        Ok(ret)
+    }
+}
 
 fn main() -> Result<(), anyhow::Error> {
     let mut config = wasmtime::Config::new();
@@ -13,15 +20,41 @@ fn main() -> Result<(), anyhow::Error> {
     let component_bytes = include_bytes!("../target/wasm32-unknown-unknown/release/pgp_wrapper.component.wasm");
     let component = wasmtime::component::Component::new(&mut engine, &component_bytes)?;
     let mut linker = wasmtime::component::Linker::new(&mut engine);
-    pgp_wrapper_imports::add_to_linker(&mut linker, |state: &mut Context| state)?;
+    get_random::add_to_linker(&mut linker, |state: &mut Context| state)?;
     let mut store = wasmtime::Store::new(&engine, Context);
     let (bindings, _instance) = PgpWrapperWorld::instantiate(&mut store, &component, &linker)?;
 
+    decrypt_example(&bindings, &mut store)?;
+    encrypt_example(&bindings, &mut store)?;
+
+    Ok(())
+}
+
+fn decrypt_example(
+    bindings: &PgpWrapperWorld,
+    store: &mut wasmtime::Store<Context>
+) -> Result<(), anyhow::Error> {
+    println!("decrypt_example");
     let enc = std::fs::read("test_data/pass/test-pass.gpg")?;
-    let key = std::fs::read_to_string("test_data/priv.key")?;
+    let priv_key = std::fs::read("test_data/priv.key")?;
     let passphrase = "foo";
-    let out: Vec<u8> = bindings.pgp_wrapper_exports.decrypt(&mut store, &*enc, &*key, passphrase)?
+    let out: Vec<u8> = bindings.pgp_wrapper_exports.decrypt(store, &*enc, &*priv_key, passphrase)?
                                .map_err(|e| anyhow::Error::msg(e))?;
     println!("out = '{out:?}'");
+
+    Ok(())
+}
+
+fn encrypt_example(
+    bindings: &PgpWrapperWorld,
+    store: &mut wasmtime::Store<Context>
+) -> Result<(), anyhow::Error> {
+    println!("encrypt_example");
+    let pub_key = std::fs::read("test_data/pub.key")?;
+    let msg = b"hello, world!";
+    let out: Vec<u8> = bindings.pgp_wrapper_exports.encrypt(store, msg, &*pub_key)?
+                               .map_err(|e| anyhow::Error::msg(e))?;
+    println!("out = '{out:?}'");
+
     Ok(())
 }
